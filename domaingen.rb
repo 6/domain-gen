b05918ccb630a5c06885447d46eb5d160d8893e6
Peter Graham <peterghm@gmail.com>
2013-07-01 00:00:38 -0700

    ability to specify starting index of search

--- domaingen.rb
+++ domaingen.rb
@@ -3,20 +3,21 @@ require 'trollop'
 
 class DomainGenerator
   attr_reader :whois, :options, :tld
-  attr_accessor :available_domains
+  attr_accessor :available_domains, :index
   def initialize(options = {})
     @whois = Whois::Client.new
     @options = options
     @tld = "." + options[:tld]
     @available_domains = []
+    @index = options[:start_index] || 0
   end
 
   def check(domain)
     if @whois.lookup(domain).available?
-      puts "#{domain} available"
+      puts "#{index}. #{domain} available"
       available_domains << domain
     else
-      puts "#{domain} unavailable"
+      puts "#{index}. #{domain} unavailable"
     end
   end
 
@@ -28,7 +29,7 @@ end
 class PermutationGenerator < DomainGenerator
   def start!
     raise StandardError.new("Specify a domain prefix/suffix")  unless options[:domain_prefix] || options[:domain_suffix]
-    letter_permutations.each_with_index do |letters, i|
+    letter_permutations[index..-1].each do |letters|
       domain = if options[:domain_suffix]
         letters + options[:domain_suffix]
       else
@@ -36,6 +37,7 @@ class PermutationGenerator < DomainGenerator
       end + tld
       check(domain)
       sleep 0.5
+      @index += 1
     end
     before_exit
   end
@@ -54,6 +56,7 @@ opts = Trollop::options do
   opt :domain_prefix, 'Domain prefix', :type => :string
   opt :domain_suffix, 'Domain suffix', :type => :string
   opt :permutation_length, 'Suffix/prefix permutation length', :type => :integer, :default => 1
+  opt :start_index, 'Starting point of search', :type => :integer, :default => 0
 end
 
 PermutationGenerator.new(opts).start!
