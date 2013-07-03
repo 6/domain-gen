ac0732dc79cfed3724538c2aab8b18c93f89d7aa
Peter Graham <peterghm@gmail.com>
2013-07-02 23:53:01 -0700

    dictionary-based generator

--- domaingen.rb
+++ domaingen.rb
@@ -19,6 +19,7 @@ class DomainGenerator
     @tld = "." + options[:tld]
     @available_domains = []
     @index = options[:start_index] || 0
+    raise StandardError.new("Specify a domain prefix/suffix")  unless options[:domain_prefix] || options[:domain_suffix]
   end
 
   def check(domain)
@@ -39,7 +40,6 @@ end
 
 class PermutationGenerator < DomainGenerator
   def start!
-    raise StandardError.new("Specify a domain prefix/suffix")  unless options[:domain_prefix] || options[:domain_suffix]
     letter_permutations[index..-1].each do |letters|
       domain = if options[:domain_suffix]
         letters + options[:domain_suffix]
@@ -62,12 +62,39 @@ class PermutationGenerator < DomainGenerator
   end
 end
 
+class DictionaryGenerator < DomainGenerator
+  def start!
+    words.each do |word|
+      domain = if options[:domain_suffix]
+        word + options[:domain_suffix]
+      else
+        options[:domain_prefix] + word
+      end + tld
+      check(domain)
+      sleep 1
+      @index += 1
+    end
+    before_exit
+  end
+
+  private
+
+  def words
+    @words ||= JSON.parse(File.read('./xkcd_words.json'))
+  end
+end
+
 opts = Trollop::options do
   opt :tld, "Top-level domain (e.g. 'org')", :type => :string, :default => 'com'
   opt :domain_prefix, 'Domain prefix', :type => :string
   opt :domain_suffix, 'Domain suffix', :type => :string
   opt :permutation_length, 'Suffix/prefix permutation length', :type => :integer, :default => 1
   opt :start_index, 'Starting point of search', :type => :integer, :default => 0
+  opt :type, 'Type of domain generation to use: `permutation` or `dictionary`', :type => :string
 end
 
-PermutationGenerator.new(opts).start!
+if opts[:type] == 'dictionary'
+  DictionaryGenerator.new(opts).start!
+else
+  PermutationGenerator.new(opts).start!
+end
